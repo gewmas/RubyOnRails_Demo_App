484d4356814ac4c3cc751a379a9baff803f8c823
Yuhua Mai <3534322+myhgew@users.noreply.github.com>
2020-12-19 00:04:54 -0800

    Upgrade Rails version (#1)
    * Update Gemfile
    
    * Add manifest.js
    
    * Fix db migration
    
    * Fix tests
    
    * Add rubocop to fix format
    
    * Update format

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-demo_app
+Demo App
 ========
 
 Ruby On Rails
+
+## Commands
+
+### Database
+
+```
+rails db:migrate
+rails db:seed
+
+rails c # rails console
+
+> Micropost.last.user
+> User.count
+```
+
+### Server
+
+```
+rails server
+
+rails test
+```
+
+### Format
+
+```
+rubocop --require rubocop-rails
+rubocop -a # rubocop --auto-correct
+```
+
+## Reference
+
+- [How to Upgrade Rails app](https://redgreenrepeat.com/2020/07/03/how-to-upgrade-rails-app/)
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -5,43 +5,43 @@ class MicropostsControllerTest < ActionController::TestCase
     @micropost = microposts(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:microposts)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create micropost" do
+  test 'should create micropost' do
     assert_difference('Micropost.count') do
-      post :create, micropost: { content: @micropost.content, user_id: @micropost.user_id }
+      post :create, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
     end
 
     assert_redirected_to micropost_path(assigns(:micropost))
   end
 
-  test "should show micropost" do
-    get :show, id: @micropost
+  test 'should show micropost' do
+    get :show, params: { id: @micropost }
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @micropost
+  test 'should get edit' do
+    get :edit, params: { id: @micropost }
     assert_response :success
   end
 
-  test "should update micropost" do
-    patch :update, id: @micropost, micropost: { content: @micropost.content, user_id: @micropost.user_id }
+  test 'should update micropost' do
+    patch :update, params: { id: @micropost, micropost: { content: @micropost.content, user_id: @micropost.user_id } }
     assert_redirected_to micropost_path(assigns(:micropost))
   end
 
-  test "should destroy micropost" do
+  test 'should destroy micropost' do
     assert_difference('Micropost.count', -1) do
-      delete :destroy, id: @micropost
+      delete :destroy, params: { id: @micropost }
     end
 
     assert_redirected_to microposts_path
